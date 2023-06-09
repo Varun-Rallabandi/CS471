8e457df5838c2fb092b5b93341a05cf4df49b60a
mdamireddy <dami0446@kettering.edu>
2023-06-09 11:46:05 -0400

    Implementation code for Auto-Email

--- app.py
+++ app.py
@@ -5,6 +5,33 @@ from flask import Flask, render_template, json, request, session, redirect
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
+#Code for Email Implementation
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'tutorg76@gmail.com'
+email_password = 'agmssublzuxxrany'
+
+email_receiver = 'lope8204@kettering.edu'
+
+subject = "You got hacked! :)"
+body = """
+You need to send $10000 to TutorG 5000 Company. Jk this is yo boi testing if this works. 
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+     smtp.login(email_sender, email_password)
+     smtp.sendmail(email_sender, email_receiver, em.as_string())
+
 #DEFINING APP
 app = Flask(__name__)
 
