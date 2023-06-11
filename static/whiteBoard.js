a6ad7dc8fcb1524b2bc3b429dc0b044c2ba03242
erins98 <67079589+erins98@users.noreply.github.com>
2023-06-11 17:54:39 -0400

    updated whiteBoard to follow the same format as other pages

--- static/whiteBoard.js
+++ static/whiteBoard.js
@@ -1,31 +1,42 @@
-const ctx = document.getElementById('canvas').getContext('2d');
-window.addEventListener('resize', resize);
-resize();
-let mousePos = {
-    x: 0,
-    y: 0
-}
-window.addEventListener('mousemove', draw);
-window.addEventListener('mousedown', mousePosition);
-window.addEventListener('mouseenter', mousePosition);
+const canvas = document.getElementById('whiteboard');
+const ctx = canvas.getContext('2d');
+const colorPicker = document.getElementById('colorPicker');
+let currentColor = 'black';
 
-function mousePosition(e) {
-    mousePos.x = e.clientX;
-    mousePos.y = e.clientY;
-}
-function resize() {
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-}
-function draw(e) {
-    if(e.buttons !== 1) return;
-    ctx.beginPath();
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = '#111';
-    ctx.linewidth = 5;
-    ctx.moveTo(mousePos.x, mousePos.y);
-    mousePosition(e);
-    ctx.lineTo(mousePos.x, mousePos.y);
-    ctx.stroke();
-}
+let draw = false;
 
+canvas.addEventListener('mousedown', (e) => {
+  draw = true;
+  drawLine(e);
+});
+
+canvas.addEventListener('mouseup', () => {
+  draw = false;
+  ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', drawLine);
+
+colorPicker.addEventListener('change', (e) => {
+  currentColor = e.target.value;
+});
+
+document.getElementById('clear').addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.getElementById('back').addEventListener('click', () => {
+  window.location.href = '/';  // Assuming 'index.html' is the main page
+});
+
+function drawLine(e) {
+  if (!draw) return;
+  ctx.lineWidth = 5;
+  ctx.lineCap = 'round';
+  ctx.strokeStyle = currentColor;
+
+  ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+}
